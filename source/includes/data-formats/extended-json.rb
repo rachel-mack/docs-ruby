eab92288d97ef8aa89f8596067f17725d43ee162
Rachel Mackintosh <rachel.mackintosh@mongodb.com>
2025-07-28 12:37:46 -0400

    JB feedback

--- source/includes/data-formats/extended-json.rb
+++ source/includes/data-formats/extended-json.rb
@@ -29,13 +29,9 @@ hash_array = [
     { "number" => BSON::Int64.new(42) }
 ]
 
-json_array_canonical = hash_array.map(&:as_extended_json)
-json_array_relaxed = hash_array.map{ |hash| hash.as_extended_json(mode: :relaxed) }
-json_array_legacy = hash_array.map{ |hash| hash.as_extended_json(mode: :legacy) }
-
-json_string_canonical = JSON.generate(json_array_canonical)
-json_string_relaxed = JSON.generate(json_array_relaxed)
-json_string_legacy = JSON.generate(json_array_legacy)
+json_string_canonical = hash_array.as_extended_json
+json_string_relaxed = hash_array.as_extended_json(mode: :relaxed)
+json_string_legacy = hash_array.as_extended_json(mode: :legacy)
 
 puts "canonical:\t #{json_string_canonical}"
 puts "relaxed:\t #{json_string_relaxed}"
